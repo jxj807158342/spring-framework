114975e644b912f8ab6680aed689ab645ea214c9
ajie <ajie123456>
2021-07-12 09:30:45 +0800

    spring 学习

--- spring-mytest/src/main/java/com/ajie/Test.java
+++ spring-mytest/src/main/java/com/ajie/Test.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("config.xml");
-
+		SimpleBean bean = classPathXmlApplicationContext.getBean(SimpleBean.class);
+		bean.sendMessage();
 	}
 }
